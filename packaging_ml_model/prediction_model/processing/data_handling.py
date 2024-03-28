c9236e699a4e39bf06cf49a680d7ae831ed63874
Ritik <ritik09052000@gmail.com>
2024-03-28 11:57:13 +0530

    added folder structure with config and datahandling

---
+++ packaging_ml_model/prediction_model/processing/data_handling.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+import joblib
+from prediction_model.config import config
+
+#load the dataset
+def load_dataset(file_name):
+    filepath= os.path.join(config.DATAPATH,file_name)
+
+    _data= pd.read_csv(filepath)
+    return _data
+
+#serializaion
+def save_pipeline(pipeline_to_save):
+    save_path=os.path.join(config.SAVE_MODEL_PATH,config.MODEL_NAME)
+    joblib.dump(pipeline_to_save,save_path)
+    print(f"model have been saved under the name {config.MODEL_NAME}")
+
+#deserializaion
+def load_pipeline(pipeline_to_load):
+    save_path=os.path.join(config.SAVE_MODEL_PATH,config.MODEL_NAME)
+    model_loaded=joblib.load(save_path)
+    print(f"model have been loaded")
+    return model_loaded
