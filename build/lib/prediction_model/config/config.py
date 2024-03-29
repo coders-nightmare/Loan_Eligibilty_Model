3710ea297bba3942fc322f2dfb35bd3edd8fdb11
Ritik <ritik09052000@gmail.com>
2024-03-29 08:44:51 +0530

    created package to build

---
+++ build/lib/prediction_model/config/config.py
@@ -0,0 +1,42 @@
+# placeholder for any manual cofiguration we want to specify , also for values for hyper parameter tunning, paths, features etc.
+
+import pathlib
+import os
+import prediction_model
+
+#prediction_model.__file__ returns path to __init__.py file in a package
+PACKAGE_ROOT = pathlib.Path(prediction_model.__file__).resolve().parent
+
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
+
+TRAIN_FILE = "train.csv"
+
+TEST_FILE =  "test.csv"
+
+MODEL_NAME='classfication.pkl'
+
+SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT,'trained_models')
+
+TARGET = 'Loan_Status'
+
+
+#Final features used in our model
+FEATURES = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed','ApplicantIncome', 'CoapplicantIncome', 'LoanAmount','Loan_Amount_Term', 'Credit_History', 'Property_Area']
+
+NUM_FEATURES = [ 'ApplicantIncome', 'LoanAmount',
+           'Loan_Amount_Term' ]
+
+CAT_FEATURES = ['Gender', 'Married', 'Dependents','Education',
+           'Self_Employed', 'Credit_History','Property_Area' ]
+
+# In our case it is same as categorical features
+FEATURES_TO_ENCODE=['Gender', 'Married', 'Dependents','Education',
+           'Self_Employed', 'Credit_History','Property_Area' ]
+
+FEATURE_TO_MODIFY=['ApplicantIncome']
+FEATURE_TO_ADD= 'CoapplicantIncome'
+
+DROP_FEATURES=['CoapplicantIncome']
+
+LOG_FEATURES=['ApplicantIncome', 'LoanAmount'] # TAKING LOG OF NUMERICAL FEATUERS
+
---
+++ build/lib/prediction_model/predict.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import joblib 
+
+from prediction_model.config import config
+
+from prediction_model.processing.data_handling import load_pipeline,load_dataset
+
+
+#loading pipeline of classification
+classification_pipeline= load_pipeline(config.MODEL_NAME)
+
+
+def generate_prediction(data_input):
+    data=pd.DataFrame(data_input)
+    pred=classification_pipeline.predict(data[config.FEATURES])
+    output=np.where(pred==1,'Y','N')
+    result={"Predictions":output}
+    return result
+
+# def generate_prediction():
+#     test_data=load_dataset(config.TEST_FILE)
+#     pred=classification_pipeline.predict(test_data[config.FEATURES])
+#     output=np.where(pred==1,'Y','N')
+#     result={"Predictions":output}
+#     print(result)
+#     return result
+
+
+if __name__=='__main__':
+    generate_prediction()
+
+
+# set PYTHONPATH=%PYTHONPATH%;E:\MLOps Udemy\LoanEligibility\packaging_ml_model
